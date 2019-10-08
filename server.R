c26cf151762160201677bf82c362401624a01292
Kevin Wang <kevin.wang@sydney.edu.au>
2019-10-08 17:16:17 +1100

    action button activates computation, improved graphics

--- server.R
+++ server.R
@@ -1,11 +1,11 @@
-shinyServer(function(input, output) {
-    gen_data <- reactive({
+server <- (function(input, output) {
+    gen_data <- eventReactive(input$compute, {
         n = input$n_samples
         p = input$n_variables
         k = 2
         rho = 0.5
         
-        set.seed(1)
+        # set.seed(1)
         
         rows = matrix(rep(c(1:p), p), nrow = p, byrow = F)
         cols = t(rows)
@@ -27,24 +27,44 @@ shinyServer(function(input, output) {
     
     
     output$ls_plot <- renderPlot({
-        plot(lm.fit(x = cbind(Int = 1, gen_data()$x), y = gen_data()$y)$coef[-1])
+        # plot(lm.fit(x = cbind(Int = 1, gen_data()$x), y = gen_data()$y)$coef[-1])
+        plotdf = tibble(
+            variables = colnames(gen_data()$x),
+            coef = lm.fit(x = cbind(Int = 1, gen_data()$x), y = gen_data()$y)$coef[-1],
+            colour = variables %in% c("X01", "X02"))
+        
+        plotdf %>% 
+            ggplot(aes(x = variables,
+                       y = coef,
+                       colour = colour,
+                       label = variables))+
+            geom_point() +
+            geom_label() +
+            scale_color_manual(values = c("black", "red")) +
+            theme(legend.position = "none",
+                  axis.ticks.x = element_blank(),
+                  axis.text.x = element_blank())
     })
     
     output$lasso_plot <- renderPlot({
         lasso = glmnet::cv.glmnet(
             x = gen_data()$x,
-            y = gen_data()$y, 
-            lambda = 10^seq(1, -5, length = 100))
+            y = gen_data()$y
+            # lambda = 10^seq(1, -5, length = 100)
+            )
         
         lasso %>% 
             coef2Df %>% 
+            dplyr::mutate(colour = variables %in% c("X01", "X02")) %>% 
             ggplot(aes(x = log(lambda), 
                        y = coef,
-                       colour = variables,
+                       colour = colour,
+                       group = variables,
                        label = variables)) +
             geom_line() +
             scale_y_continuous(breaks = c(-0.2, 0, 0.2, 0.4, 0.6, 0.8, 1)) +
-            geom_vline(xintercept = log(lasso$lambda.min)) +
+            scale_color_manual(values = c("black", "red")) +
+            geom_vline(xintercept = log(lasso$lambda.min), colour = "blue") +
             geom_dl(method = "angled.boxes") +
             theme(legend.position = "none")
     })
@@ -53,18 +73,21 @@ shinyServer(function(input, output) {
         ridge = glmnet::cv.glmnet(
             x = gen_data()$x,
             y = gen_data()$y, 
-            lambda = 10^seq(1, -5, length = 100),
+            # lambda = 10^seq(1, -5, length = 100),
             alpha = 0)
         
         ridge %>% 
             coef2Df %>% 
+            dplyr::mutate(colour = variables %in% c("X01", "X02")) %>% 
             ggplot(aes(x = log(lambda), 
                        y = coef,
-                       colour = variables,
+                       colour = colour,
+                       group = variables,
                        label = variables)) +
             geom_line() +
             scale_y_continuous(breaks = c(-0.2, 0, 0.2, 0.4, 0.6, 0.8, 1)) +
-            geom_vline(xintercept = log(ridge$lambda.min)) +
+            scale_color_manual(values = c("black", "red")) +
+            geom_vline(xintercept = log(ridge$lambda.min), colour = "blue") +
             geom_dl(method = "angled.boxes") +
             theme(legend.position = "none")
     })
--- ui.R
+++ ui.R
@@ -2,10 +2,12 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-
+    
     # Application title
     titlePanel("Linear regression estimation"),
-
+    h3("True coefficients of X01 and X02 will always be generated as 1."),
+    h3("Other variables are generated with 0."),
+    
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
@@ -19,15 +21,17 @@ shinyUI(fluidPage(
                         "Number of variables",
                         min = 5,
                         max = 50,
-                        value = 10)
-            # actionButton("compute", "Compute result"),
+                        value = 10),
+            actionButton("compute", "Compute result")
         ),
-
+        
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("ls_plot"),
-            plotOutput("lasso_plot"),
-            plotOutput("ridge_plot")
-        )
+            tabsetPanel(type = "tabs",
+                        tabPanel("Least squares", plotOutput("ls_plot")),
+                        tabPanel("Lasso", plotOutput("lasso_plot")),
+                        tabPanel("Ridge", plotOutput("ridge_plot"))
+            ) ## End tablsetPanel
+        ) ## End mainPanel
     )
 ))
